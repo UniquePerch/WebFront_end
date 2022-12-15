41581dd0a5f6316ee1aa7e79a3bb603912d5bf08
UniquePerch <92765498+UniquePerch@users.noreply.github.com>
2022-12-15 11:53:39 +0800

    Update loginscript.js

--- js/loginscript.js
+++ js/loginscript.js
@@ -2,7 +2,7 @@ function login() {
     console.log("func");
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username == 'WILLOW' && password == 'LYH701721')
+    if (username == 'willow' && password == 'lyh701721')
         window.location.href = 'index.html'
     else
         window.alert("密码输入错误")
